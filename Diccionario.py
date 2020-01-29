a64564dc5b632dd98684d3882208fa87ce0fa121
neithaltair <neith.altair.na@gmail.com>
2020-01-29 04:07:49 +0100

    Cap 9

--- Diccionario.py
+++ Diccionario.py
@@ -1,35 +1,46 @@
-dicColors = {'blue':'azul','red':'rojo','yellow':'amarillo', 'black':'negro'}
+#nombre para el diccionario, siempre va dentro de llaves
+miDicccionario={"Colombia":"Bogota", "Argentina":"Buenos Aires", "Chile":"Santiago", "Bolivia":"La Paz", "Ecuador":"Quito"}
 
-print(dicColors)
+#como acceder a un valor en concreto del diccionario
+print(miDicccionario["Colombia"])
+print(miDicccionario["Ecuador"])
 
-# Print for the user the "key"
-print(dicColors['blue']) 
+#Anexar una nueva clave al diccionario
+miDicccionario["Uruguay"] = "Caracas"
 
-# Assign the key in a variable
-valor = dicColors["red"]
+#imprimir todo el diccionario
+print("\n",miDicccionario)
 
-print(valor)
+#Cambiar el valor de la clave 
+miDicccionario["Uruguay"] = "Montevideo"
 
-# Add a new key in the dictionary
-dicColors["black"] = "negro"
+print("\n",miDicccionario)
 
-print(dicColors)
+#Eliminar elementos del diccionario
+del miDicccionario["Chile"]
 
-# Remove a key
-dicColors.pop("yellow")
+print("\n Imprime diccionario despues de eliminar a chile = ",miDicccionario)
 
-print(dicColors) 
+# Los diccionarios pueden contener todo tipo de 
+miDicccionario1 = {23 : "Jordan", "Neymar": 10}
 
-# Print the keys 
-for content in dicColors:
-    print(content)
-
-# Print the keys and the values
-for key, values in dicColors.items():
-    print("The key and the value ",key+" = "+values)
+print("\n Los diccionarios pueden contener booleanos, enteros, strings etc = ",miDicccionario1)
 
 
+#Es posible usar una tupla para asignar las llaves a los valores
+miTupla = [1,2,3,4,5,6]
+miDicccionario2 = {miTupla[0]:"Keylor", miTupla[1]:"Keylora", miTupla[2]:"Keylorb", miTupla[3]:"Keylorc", miTupla[4]:"Keylord", miTupla[5]:"Keylore"}
+
+print("\n Uso de tupla para la creacion de claves en el diccionario",miDicccionario2)
 
 
+#Para acceder en un elemento en concreto
+print(miDicccionario2[2])
 
 
+#Crear una tupla dentro de un diccionario
+miDicccionario3 = {"Equipo":"Millos", "Estrellas":"15", "Campeonatos":[2012,2016,545,1998,58877,54545]}
+
+print("\n",miDicccionario3)
+print("Campeonatos = ", miDicccionario3["Campeonatos"])
+print("Equipo = ", miDicccionario3["Equipo"])
