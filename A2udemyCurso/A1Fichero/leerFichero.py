b08ac2e14de3dc874bb6a5922e35b8696820d322
neithaltair <neith.altair.na@gmail.com>
2020-03-25 14:30:21 -0500

    Nombres carpetas, cap52

---
+++ A2udemyCurso/A1Fichero/leerFichero.py
@@ -0,0 +1,10 @@
+#se abrira un archivo de texto en modo lectura "r"
+#que contiene texto (caracteres) "t"
+#Metodo open devuelve los datos del texto y se almacenan en la variable fichero
+fichero = open("texto.txt","rt")
+
+#para leer el contenido del texto se debe llamar el metodo read
+#se asigna el contenido del texto a la variable datos_fichero
+datos_fichero = fichero.read()
+
+print(datos_fichero)
