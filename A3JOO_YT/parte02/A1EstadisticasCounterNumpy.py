afeccdaa445bab5bda86de1d6407ac5bce7ed0f1
neithaltair <neith.altair.na@gmail.com>
2020-07-28 05:30:55 +0200

    Conteo de Ocurrencias de cadenas de caracteres con la clase Counter

--- A3JOO_YT/parte02/A1EstadisticasCounterNumpy.py
+++ A3JOO_YT/parte02/A1EstadisticasCounterNumpy.py
@@ -7,3 +7,21 @@ listaLetras = ['t', 'u', 't', 't', 'u', 't', 't', 'u', 't']
 
 contador = Counter(listaLetras)
 print(contador)
+
+print()
+
+#Mas usos del Counter()
+dictLetras = {'t':3, 'u':1, 'w':2, 'x':2, 'y':2, 'z':1}
+contador=Counter(dictLetras)
+print(contador)
+
+print()
+
+contador = Counter(t=3, u=1, w=2, x=2, z=1)
+print(contador)
+
+print()
+
+cadena = 'neithaltairmachucabernal'
+contador = Counter(cadena)
+print(contador)
