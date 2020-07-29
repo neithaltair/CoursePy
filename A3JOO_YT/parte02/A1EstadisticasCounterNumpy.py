213d35d1a879d83ead733ce7312078c8f6295e62
neithaltair <neith.altair.na@gmail.com>
2020-07-29 03:20:50 +0200

    Varias operaciones Interesantes de un Objeto Counter

--- A3JOO_YT/parte02/A1EstadisticasCounterNumpy.py
+++ A3JOO_YT/parte02/A1EstadisticasCounterNumpy.py
@@ -25,3 +25,28 @@ print()
 cadena = 'neithaltairmachucabernal'
 contador = Counter(cadena)
 print(contador)
+
+print()
+
+#31
+#Varias operaciones interesantes de un Objeto Counter
+#Numero de ocurrencias de la a en la variable contador referente a cadena
+print(contador['a'])
+print(contador)
+
+#Realiza la impresion del top 3 de las letras mas comunes en la variable
+print("\n", contador.most_common(3))
+
+print()
+#OPERACIONES DE ITERACION
+for k, v in contador.most_common():
+    print("Llave: {} - Valor: {}".format(k, v))
+
+print()
+
+#Realizar la iteracion en una sola linea, SOLO IMPRIME LA CANTIDAD DE VECES
+#QUE SE REPITEN LAS LETRAS PERON NO MUESTRA CUALES LETRAS
+conteos = [v for k, v in contador.most_common()]
+print(len(conteos))
+print(conteos)
+ 
